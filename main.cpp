1ca177673995187c638dd4a8aed91c1258e4c7bc
Stanislas Michalak <stanislas.michalak@gmail.com>
2012-10-23 20:16:16 +0200

    Ajout du main

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Stanislas Michalak <stanislas.michalak@gmail.com>
+ *
+ * Created on 23 octobre 2012, 20:12
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+   std::cout << "Hello world" << std::endl;
+   
+   return EXIT_SUCCESS;
+}
+
