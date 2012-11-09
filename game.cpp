770a5b37adeb501971a6eec6d183655a5ce850bf
Stanislas Michalak <stanislas.michalak@gmail.com>
2012-11-09 09:34:06 +0100

    Ajout des sources pour le module "game"

---
+++ game.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   game.cpp
+ * Author: Stanislas Michalak <stanislas.michalak@gmail.com>
+ *
+ * Created on 9 novembre 2012, 9:30
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+/*
+ * 
+ */
+
+bool play() {
+   return true;
+}
---
+++ game.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   game.h
+ * Author: Stanislas Michalak <stanislas.michalak@gmail.com>
+ *
+ * Created on November 9, 2012, 9:32 AM
+ */
+
+#ifndef GAME_H
+#define	GAME_H
+
+bool play();
+
+#endif	/* GAME_H */
+
