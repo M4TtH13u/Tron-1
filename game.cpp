e1bab3ba341b2015457b8dc4c8bf97032055fde4
Matthieu <kowaleswsk@eisit.eu>
2012-11-13 16:49:57 +0100

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -13,5 +13,26 @@
  */
 
 bool play() {
-   return true;
+  do {
+      std::cout << "Tron" << std::endl << std::endl;
+      std::cout << "1 - Joueur vs IA" << std::endl;
+      std::cout << "2 - Joueur vs Joueur" << std::endl;
+      std::cout << "3 - Retour menu" << std::endl;
+      std::cin >> choix;
+
+      switch (choix) {
+         case 1:
+            //partie Vs IA
+            std::cout << "Joueur vs IA" << std::endl;
+            break;
+         case 2:
+            //Partie Vs Joueur
+            std::cout << "Joueur vs Joueur" << std::endl;
+            break;
+         default:
+            continue;
+            break;
+      }
+   } while (choix != 3);
+return true;
 }
